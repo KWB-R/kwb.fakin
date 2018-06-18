3ff25a66759af381ed3a3ce544f9e3eed5d9a7b6
mrustl <michael.rustler@kompetenz-wasser.de>
2018-06-19 01:11:18 +0200

    moved package build functions to R package kwb.pkgbuild (see: https://github.com/KWB-R/kwb.pkgbuild)

--- R/use_codecov.R
+++
@@ -1,9 +0,0 @@
-# use_codecov ----------------------------------------------------------------
-#' Adds codecov.yml
-#' @return writes codecov.yml and adds it .Rbuildignore
-#' @importFrom usethis use_coverage
-#' @export
-use_codecov <- function() {
-
- usethis::use_coverage(type = "codecov")
-}
