94b0fa096ffcdde01e65679ed39b032f1e838bee
hsonne <hauke.sonnenberg@gmx.de>
2018-09-12 16:27:41 +0200

    Rewrite missing function get_path_properties()

--- inst/extdata/extract_properties.R
+++ inst/extdata/extract_properties.R
@@ -3,12 +3,14 @@
 
 library("kwb.utils")
 
-file <- "Desktop/Data/FAKIN/paths_poseidon_projekte.txt"
+#file <- "Desktop/Data/FAKIN/paths_poseidon_projekte.txt"
 #file <- "~/Desktop/tmp/paths_projekte_2.txt"
+file <- "~/Desktop/Data/FAKIN/folders_projects/folders_projects_2018-09-11.txt"
 
 pkg_file <- function(name) system.file("extdata", name, package = "kwb.fakin")
 file_composed <- pkg_file("replacements_composed-words.csv")
-file_attribute_words <- pkg_file("words-to-attributes.csv")
+#file_attribute_words <- pkg_file("words-to-attributes.csv")
+file_attribute_words <- pkg_file("words-to-attributes_aquanes.csv")
 
 # MAIN -------------------------------------------------------------------------
 if (FALSE)
@@ -19,19 +21,21 @@ if (FALSE)
   # kwb.fakin:::store(paths, "extract_properties")
   # paths <- kwb.fakin:::restore("paths")
 
-  folder_paths <- unique(dirname(paths))
+  #folder_paths <- unique(dirname(paths))
+  folder_paths <- paths
 
   path_tree <- kwb.fakin:::to_tree(folder_paths)
 
   # kwb.fakin:::store(path_tree, "extract_properties")
-  # path_tree <- kwb.fakin:::restore("path_tree")
+  # path_tree <- kwb.fakin:::restore("path_tree", 2)
 
-  subtree <- path_tree$SUW_Department$Projects
+  #subtree <- path_tree$SUW_Department$Projects
+  subtree <- path_tree$WWT_Department$Projects
 
-  first_levels <- get_first_levels(tree = subtree, 2)
+  first_levels <- kwb.fakin:::get_first_levels(tree = subtree, 2)
 
   # Get all unique folder names
-  folder_names <- sort_unique(unlist(first_levels))
+  folder_names <- kwb.fakin:::sort_unique(unlist(first_levels))
 
   # Prepare file for replacements for composed words
   # print_replacement_template(sort_unique(folder_names))
@@ -44,10 +48,10 @@ if (FALSE)
 
   # print_replacement_template(sort_unique(folder_names_2))
 
-  property_defs <- get_words_to_attribute_list(file = file_attribute_words)
+  property_defs <- kwb.fakin:::get_words_to_attribute_list(file = file_attribute_words)
 
-  folder_properties <- extract_properties(
-    x = folder_names_3,
+  folder_properties <- kwb.fakin:::extract_properties(
+    x = folder_names, # folder_names_3
     patterns = property_defs$word,
     replacements = property_defs$attributes,
     as_data_frame = FALSE
@@ -107,4 +111,32 @@ if (FALSE)
   x <- c("noch viel mehr key_abc", "key_abd", "abe")
 
   extract_and_substitute("key_(ab(c|d))", "alphabet:true+full:\\1+char2:\\2", x)
+}
+
+# get_path_properties (rewritten, where is the original?) ----------------------
+get_path_properties <- function(subtree, folder_properties)
+{
+  path_part_list <- kwb.fakin:::splitPaths(kwb.fakin:::flatten_tree(subtree$AquaNES))
+
+  remove_na_or_empty <- function(x) x[! kwb.utils::isNaOrEmpty(x)]
+
+  property_strings <- sapply(path_part_list, function(path_parts) {
+
+    paste(collapse = "+", kwb.fakin:::sort_unique(remove_na_or_empty(
+      folder_properties[path_parts]
+    )))
+  })
+
+  # property_list <- lapply(property_strings, function(property_string) {
+  #
+  #   key_values <- kwb.utils::toKeysAndValues(property_string, c("[+]", ":"))
+  #
+  #   result <- do.call(kwb.utils::toLookupTable, key_values)
+  #
+  #   stats::setNames(result, kwb.utils::makeUnique(names(result), warn = FALSE))
+  # })
+  #
+  # as.matrix(kwb.utils::safeRowBindAll(property_list))
+
+  property_strings
 }
