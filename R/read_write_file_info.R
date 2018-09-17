c0304873ac4f9fa046ecdba98010dfb92c7380d1
Hauke Sonnenberg <hauke.sonnenberg@kompetenz-wasser.de>
2018-09-17 17:22:40 +0200

    Add documentation and export file info IO functions

--- R/read_write_file_info.R
+++ R/read_write_file_info.R
@@ -1,6 +1,16 @@
 # write_file_info --------------------------------------------------------------
 
-# Write the file information to a CSV file
+#' Write File Information to CSV File
+#'
+#' @param file_info data frame as returned by
+#'   \code{\link{get_recursive_file_info}}
+#' @param file path to CSV file to be written
+#' @param version determines which function to use for writing the CSV file
+#'   1: \code{\link[utils]{write.table}}, 2: \code{\link[data.table]{fwrite}}
+#'
+#' @export
+#'
+
 write_file_info <- function(file_info, file, version = 2)
 {
   kwb.utils::catAndRun(paste("Writing", file), {
@@ -41,7 +51,15 @@ write_file_info_v2 <- function(file_info, file)
 
 # read_file_info ---------------------------------------------------------------
 
-# Read the file information back into R
+#' Read File Information from CSV File
+#'
+#' @param file path to CSV file containing file information as stored with
+#'   \code{\link{write_file_info}}
+#' @param version determines which function to use for reading the CSV file
+#'   1: \code{\link[utils]{read.table}}, 2: \code{\link[data.table]{fread}}
+#'
+#' @export
+#'
 read_file_info <- function(file, version = 2)
 {
   kwb.utils::catAndRun(paste("Reading file information from", file), {
