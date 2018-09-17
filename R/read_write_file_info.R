5651b3f7d3a2274b5ed83212f8001b62bd330b67
Hauke Sonnenberg <hauke.sonnenberg@kompetenz-wasser.de>
2018-09-17 21:10:09 +0200

    Add arg "check_dirs" to get_and_save_file_info()
    Do not check if input and output directories
    exist. An existing network drive was considered
    to be non-exising...

--- R/read_write_file_info.R
+++ R/read_write_file_info.R
@@ -8,13 +8,20 @@
 #'   "path-info_<date-time>_<parent-folder>.csv" will be generated with
 #'   <date-time> being a date and time string in yyyy-mm-dd_HHMM format and
 #'   <parent-folder> being the last path segment of \code{root_dir}
+#' @param check_dirs if \code{TRUE} (default) it is checked in advance if both
+#'   \code{root_dir} and \code{output_dir} exist. Switch this off if e.g.
+#'   network paths are wrongly considered to be non-existing.
 #'
 #' @export
 #'
-get_and_save_file_info <- function(root_dir, output_dir)
+get_and_save_file_info <- function(root_dir, output_dir, check_dirs = TRUE)
 {
   # Check if the root directory exists
-  kwb.utils::safePath(root_dir)
+  if (check_dirs) {
+
+    kwb.utils::safePath(root_dir)
+    kwb.utils::safePath(output_dir)
+  }
 
   # Get information on all files in this directory
   runtime <- system.time(file_info <- get_recursive_file_info(root_dir))
@@ -28,7 +35,7 @@ get_and_save_file_info <- function(root_dir, output_dir)
 
   filename <- sprintf("path-info_%s_%s.csv", datetime_string, parent_folder)
 
-  file <- file.path(kwb.utils::safePath(output_dir), filename)
+  file <- file.path(output_dir, filename)
 
   # Write the file information to a CSV file
   write_file_info(file_info, file)
