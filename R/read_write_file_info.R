c326d2d841de9e03fd4fa92c1514ba2c945334a0
hsonne <hauke.sonnenberg@gmx.de>
2018-09-20 08:02:59 +0200

    New function: read_path_information()

--- R/read_write_file_info.R
+++ R/read_write_file_info.R
@@ -1,3 +1,13 @@
+# read_path_information --------------------------------------------------------
+read_path_information <- function(file_info_dir)
+{
+  files <- dir(file_info_dir, "^path-info", full.names = TRUE)
+
+  names(files) <- kwb.fakin:::extract_root_name(files)
+
+  lapply(files, kwb.fakin::read_file_info)
+}
+
 # get_and_save_file_info -------------------------------------------------------
 
 #' Get and Save File Information
