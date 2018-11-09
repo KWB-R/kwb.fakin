dd6aa036cc9f1ea3bae7a1b3e653dd41377c3d43
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2018-11-09 22:11:07 +0100

    Add script to generate property availability

---
+++ inst/extdata/scripts/write_file_property_availability.R
@@ -0,0 +1,25 @@
+#
+# Update info on property availablility in file "property_names.txt"
+#
+
+file <- system.file("extdata", "property_names.txt", package = "kwb.fakin")
+
+property_info <- read.table(
+  file, sep = ",", header = TRUE, stringsAsFactors = FALSE
+)
+
+property_names <- property_info$property
+
+# Filter for valid property names that can be used in a Search Index SQL query
+available <- property_is_available(property_names)
+
+property_info <- kwb.utils::noFactorDataFrame(
+  property = property_names,
+  available = available
+)
+
+property_file <- file.path(kwb.utils::desktop(), "tmp", "property_names.txt")
+
+write.table(property_info, property_file, sep = ",", row.names = FALSE)
+
+kwb.utils::hsOpenWindowsExplorer(dirname(property_file))
