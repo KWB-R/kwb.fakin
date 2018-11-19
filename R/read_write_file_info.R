6371e2459c4c8f5bfc307f25f058016a6c46b6ab
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2018-11-19 18:10:05 +0100

    Consider new file format in read_file_info()
    - Allow to read CSV file that has been generated
      with Get-ChildItem()-call in Windows PowerShell-
      Skript. Provide columns "path", "type", "size".
    - Use new function reformat_file_info() to do the
      reformatting.
    - Suggest package "lubridate" (used to convert
      timestamp columns)

--- R/read_write_file_info.R
+++ R/read_write_file_info.R
@@ -179,7 +179,18 @@ read_file_info <- function(file, version = 2, ...)
 
   cat_elapsed(time_info)
 
-  file_info
+  # If the file has been created with PowerShell, adapt the format
+  if ("FullName" %in% names(file_info)) {
+
+    kwb.utils::catAndRun(
+      "Reformatting the file info table",
+      reformat_file_info(file_info)
+    )
+
+  } else {
+
+    file_info
+  }
 }
 
 # read_file_info_v1 ------------------------------------------------------------
@@ -192,4 +203,33 @@ read_file_info_v1 <- function(file, sep = ";")
 read_file_info_v2 <- function(file, sep = ";")
 {
   as.data.frame(data.table::fread(file = file, sep = sep))
+}
+
+# reformat_file_info -----------------------------------------------------------
+reformat_file_info <- function(file_info)
+{
+  # Which columns are time columns?
+  is_time <- grepl("Utc$", names(file_info))
+
+  # Convert character timestamps to POSIXct
+  file_info[is_time] <- lapply(file_info[is_time], lubridate::dmy_hms)
+
+  # Set column "type" to either "file" or "directory"
+  attribs <- kwb.utils::selectColumns(file_info, "Attributes")
+  file_info$type <- ifelse(grepl("Directory", attribs), "directory", "file")
+
+  # Rename columns
+  file_info <- kwb.utils::renameColumns(file_info, list(
+    Length = "size", FullName = "path"
+  ))
+
+  # Set the path delimiters to forward slashes
+  paths <- kwb.utils::selectColumns(file_info, "path")
+  file_info$path <- kwb.utils::rStylePath(paths)
+
+  # Set NA to 0 in column size
+  file_info$size <- kwb.utils::defaultIfNA(file_info$size, 0)
+
+  # Move main columns to the front
+  kwb.utils::moveColumnsToFront(file_info, c("path", "type", "size"))
 }
