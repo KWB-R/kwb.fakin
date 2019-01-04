286b1cd3818163d746c173eef614789108468ee1
hsonne <hauke.sonnenberg@gmx.de>
2019-01-05 00:42:53 +0100

    Do not exceed 80 characters per line

--- R/dictionary.R
+++ R/dictionary.R
@@ -50,7 +50,8 @@ getDictOneByOne <- function(paths, n = 10)
 
 # getFrequencies ---------------------------------------------------------------
 getFrequencies <- function(
-  subdirs = kwb.file:::split_paths(paths), paths = NULL, first.only = TRUE, dbg = TRUE
+  subdirs = kwb.file:::split_paths(paths), paths = NULL, first.only = TRUE,
+  dbg = TRUE
 )
 {
   n_levels <- kwb.utils::getElementLengths(subdirs)
