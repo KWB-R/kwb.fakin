310332b3b15338bdb440e6b6377abfbc4ff6a11a
hsonne <hauke.sonnenberg@gmx.de>
2019-01-05 01:17:48 +0100

    Move cut_left() to utils.R and remove manipulate.R

--- R/utils.R
+++ R/utils.R
@@ -14,6 +14,24 @@ cat_time <- function(tag)
   cat(paste0("\n", tag, ":"), as.character(Sys.time()), "\n\n")
 }
 
+# cut_left ---------------------------------------------------------------------
+cut_left <- function(x, start_string)
+{
+  string_length <- nchar(start_string)
+
+  matching <- substr(x, 1, string_length) == start_string
+
+  if (! all(matching)) {
+
+    stop_(sprintf(
+      "%d strings in '%s' do not start with '%s'!",
+      sum(! matching), deparse(substitute(x)), start_string
+    ))
+  }
+
+  substr(x, string_length + 1, nchar(x))
+}
+
 # extdata_file -----------------------------------------------------------------
 extdata_file <- function(file)
 {
