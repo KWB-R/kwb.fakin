c62cc88306d030ad21b139662bb61621014a656c
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2019-03-05 10:51:52 +0100

    Fix test, use functions of available package,
    here: kwb.code is available as kwb.fakin
    depends on it.

--- inst/extdata/testcalls_1.R
+++ inst/extdata/testcalls_1.R
@@ -1,7 +1,7 @@
 # This file is used to test if the function get_package_function_usage()
 # correctly identifies calls of package functions
 if (FALSE) {
-  kwb.logger::read_aquatroll_data("xyz.txt")
+  kwb.code::parse_scripts()
 }
 
-kwb.logger::read_logger_LT_EDGE_M100()
+kwb.code::get_full_function_info()
--- tests/testthat/test-function-get_package_function_usage.R
+++ tests/testthat/test-function-get_package_function_usage.R
@@ -8,12 +8,18 @@ test_that("get_package_function_usage() works", {
 
   get_usage <- function(script) {
     tree <- kwb.code::parse_scripts(dirname(script), basename(script))
-    kwb.fakin:::get_package_function_usage(tree, package = "kwb.logger")
+    kwb.fakin:::get_package_function_usage(tree, package = "kwb.code")
   }
+
+  # Unfortunateley the package (kwb.code) needs to be on the search path
 
   usage <- lapply(scripts, get_usage)
 
-  identical(usage[[1]]$name, c("read_aquatroll_data", "read_logger_LT_EDGE_M100"))
-  identical(usage[[2]]$name, c("read_aquatroll_data"))
-  identical(usage[[3]]$name, c("read_aquatroll_data"))
+  expect_identical(usage[[1]]$name, c("get_full_function_info", "parse_scripts"))
+  expect_identical(usage[[2]]$name, c("parse_scripts"))
+  expect_identical(usage[[2]]$explicit, 0)
+  expect_identical(usage[[2]]$implicit, 1)
+  expect_identical(usage[[3]]$name, c("parse_scripts"))
+  expect_identical(usage[[3]]$explicit, 1)
+  expect_identical(usage[[3]]$implicit, 0)
 })
