fa254ad808308dc8ef914d1bc3ba1f03a5230943
hsonne <hauke.sonnenberg@gmx.de>
2019-03-23 11:21:34 +0100

    Move summary.path_tree() and sub-function to tree.R
    and delete summary.R

--- R/tree.R
+++ R/tree.R
@@ -80,3 +80,35 @@ to_tree <- function(x, dbg = FALSE)
 
   kwb.utils::addClass(trees, "path_tree")
 }
+
+# summary.path_tree ------------------------------------------------------------
+
+#' Get Statistics on Path Tree Nodes
+#'
+#' @param object tree object as returned by \code{kwb.fakin:::to_tree}
+#' @param \dots further arguments (currently not used)
+#'
+#' @export
+#'
+summary.path_tree <- function(object, ...)
+{
+  name_frequency <- table(get_names_recursively(object))
+
+  result <- as.matrix(name_frequency[order(names(name_frequency))])
+
+  dimnames(result) <- list(node_name = rownames(result), "occurrences")
+
+  result
+}
+
+# get_names_recursively --------------------------------------------------------
+get_names_recursively <- function(x)
+{
+  if (is.list(x)) {
+
+    do.call(c, lapply(names(x), function(element) {
+
+      c(element, get_names_recursively(x[[element]]))
+    }))
+  }
+}
