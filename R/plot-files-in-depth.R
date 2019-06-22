802be069ec6628de6178f1570996467b2351b3ae
hsonne <hauke.sonnenberg@gmx.de>
2019-06-22 16:06:08 +0200

    Do not support plot_file_in_depth() any more
    - remove depth_summary_to_size_in_depth(),
    - remove plot_size_in_depth()

--- R/plot-files-in-depth.R
+++ R/plot-files-in-depth.R
@@ -8,23 +8,7 @@
 #'
 plot_files_in_depth <- function(depth_summaries, project)
 {
-  # Filter each depth summary by the project
-  depth_summaries <- lapply(depth_summaries, filter_for_project, project)
-
-  # Check depth summaries
-  check_depth_summaries(depth_summaries)
-
-  # Get the most detailed summary available
-  max_depth_summary <- depth_summaries[[length(depth_summaries)]]
-  stopifnot(all(kwb.utils::selectColumns(max_depth_summary, "n_files") == 1))
-
-  # Convert to "size_in_depth" data
-  size_in_depth <- depth_summary_to_size_in_depth(max_depth_summary)
-
-  plot_size_in_depth(size_in_depth) +
-    ggplot2::ggtitle(paste("Project:", project)) +
-    ggplot2::xlab("Folder depth") +
-    ggplot2::ylab("File size in MB")
+  stop_("plot_file_in_depth() has been removed.")
 }
 
 # filter_for_project -----------------------------------------------------------
@@ -55,18 +39,6 @@ check_depth_summaries <- function(depth_summaries)
   invisible(depth_summary)
 }
 
-# depth_summary_to_size_in_depth -----------------------------------------------
-depth_summary_to_size_in_depth <- function(depth_summary)
-{
-  check_depth_summary(depth_summary)
-  level_columns <- setdiff(names(depth_summary), c("n_files", "total_size"))
-  subdirs <- kwb.utils::selectColumns(depth_summary, level_columns)
-  data.frame(
-    depth = get_depths_from_subdir_matrix(subdirs),
-    size = kwb.utils::selectColumns(depth_summary, c("total_size"))
-  )
-}
-
 # check_depth_summary ----------------------------------------------------------
 check_depth_summary <- function(depth_summary)
 {
@@ -90,16 +62,4 @@ get_depths_from_subdir_matrix <- function(subdirs)
       max_depth
     }
   })
-}
-
-# plot_size_in_depth -----------------------------------------------------------
-plot_size_in_depth <- function(size_in_depth)
-{
-  bytes_per_mb <- 1024 * 1024
-  kwb.utils::checkForMissingColumns(size_in_depth, c("depth", "size"))
-  size_in_depth$size_MB <- size_in_depth$size / bytes_per_mb
-  ggplot2::ggplot(size_in_depth, ggplot2::aes_string("depth", "size_MB")) +
-    ggplot2::geom_point() +
-    ggplot2::scale_x_continuous(breaks = 1:100) +
-    ggplot2::scale_y_log10()
 }
