8ea4454b6b98457a1a720f5e674d8c33cdb8676f
hsonne <hauke.sonnenberg@gmx.de>
2019-06-26 23:15:12 +0200

    Add comments, extract plot_top_properties_distribution()

--- inst/extdata/scripts/extract_properties.R
+++ inst/extdata/scripts/extract_properties.R
@@ -22,20 +22,27 @@ file_attribute_words <- pkg_file("config/words-to-attributes.csv")
 #file_attribute_words <- pkg_file("words-to-attributes_aquanes.csv")
 
 #Sys.setlocale(locale = "de_DE.utf-8")
-Sys.setlocale(locale = "german")
+#Sys.setlocale(locale = "german")
+
+# MAIN: Clear storage ----------------------------------------------------------
+if (FALSE)
+{
+  kwb.fakin:::clear_storage("path_tree")
+  kwb.fakin:::clear_storage("paths")
+}
 
 # MAIN -------------------------------------------------------------------------
 if (FALSE)
 {
-  # Clear storage
-  #kwb.fakin:::clear_storage("path_tree")
-  #kwb.fakin:::clear_storage("paths")
-
   # Read folder paths from a file and convert them to a tree structure. This
   # takes some time. Therefore, store() intermediate results and restore() if
-  # stored objects are available. So, running the following for the second time
-  # is much faster than running it the first time.
-  path_tree <- provide_path_tree(file)
+  # stored objects are available. So, running the following line for the second
+  # time is much faster than running it the first time.
+  # Possible file encodings: "Windows-1252", "ISO-8859-1"
+  path_tree <- provide_path_tree(file, fileEncoding = "ISO-8859-1")
+
+  # What encoding is stored for the strings?
+  table(Encoding(x <- kwb.fakin:::flatten_tree(path_tree)))
 
   # Select path trees by department
   department_trees <- list(
@@ -44,42 +51,46 @@ if (FALSE)
     GRW = path_tree$GROUNDWATER$PROJECTS
   )
 
-  # Get folder frequencies down to a certain folder depth
+  # Get folder name frequencies down to a certain folder depth
   (folder_frequencies <- lapply(department_trees, get_folder_frequency, 5))
 
+  # Which different words are used in the Groundwater department?
   (words <- rownames(folder_frequencies$GRW))
   (words <- sort(unique(unlist(strsplit(words, "\\s+")))))
 
+  # Order words by their similarity with the word "report" (most similar last)
   words[order(RecordLinkage::levenshteinSim("report", words))]
 
+  # Look for folders that may contain raw data
   grep("roh|raw", words, value = TRUE, ignore.case = TRUE)
 
   # Search the frequency tables for relevant patterns
   lapply(folder_frequencies, grep_frequency, "litera")
 
+  # Print a sub tree
   print(department_trees$SUW, 2)
 
-  # Get all unique folder names
+  # Get all unique folder names used in the Surface Water department
   folder_frequency <- folder_frequencies$SUW
   (folder_names <- rownames(folder_frequency))
 
-  # Prepare file for replacements for composed words
-  # kwb.fakin:::print_replacement_template(kwb.fakin:::sort_unique(folder_names))
-
   # Correct composed words
-
   folder_names_2 <- kwb.fakin:::apply_substitutions_from_file(
     folder_names, file_composed
   )
 
   folder_names_3 <- tolower(folder_names_2)
 
+  # Prepare file for replacements for composed words
+  # kwb.fakin:::print_replacement_template(kwb.fakin:::sort_unique(folder_names))
   # kwb.fakin:::print_replacement_template(kwb.fakin:::sort_unique(folder_names_2))
 
+  # Read assignments from word patterns to attributes
   property_defs <- kwb.fakin:::get_words_to_attribute_list(
     file = file_attribute_words
   )
 
+  # Assign attributes to matching folder names
   folder_properties <- kwb.fakin:::extract_properties(
     x = folder_names, # folder_names_3
     patterns = property_defs$word,
@@ -87,25 +98,39 @@ if (FALSE)
     as_data_frame = FALSE
   )
 
-  View(folder_properties)
+  # Show the folder names together with the assigned attributes
+  head(folder_properties[folder_properties != ""])
 
-  result <- get_path_properties(subtree$AquaNES, folder_properties)
+  # Select the Wastewater department subtree
+  wwt_paths <- path_tree$WWT_Department$Projects
 
+  # Try to extract properties from the AquaNES paths
+  result <- get_path_properties(wwt_paths, folder_properties)
+
+  # Filter for paths to which properties could be assigned
+  result <- result[lengths(result) > 0]
+
+  # Convert to YAML format and print
   cat(yaml::as.yaml(result))
 
-  frequencies <- sort(table(sapply(result, to_property_strings)))
+  # Plot the frequencies of given properties
+  plot_top_properties_distribution(result)
 
-  kwb.plot::setMargins(left = 14, top = 0, bottom = 2)
+  # Unify folder names by applying substitutions that are stored in a file
+  folder_names <- kwb.fakin:::apply_substitutions_from_file(
+    folder_names, file_unify
+  )
 
-  barplot(tail(frequencies, 30), horiz = TRUE, cex.names = 0.6, las = 1)
+  # Show the ordered list of unified folder names
+  kwb.fakin:::sort_unique(folder_names)
 
-  folder_names <- apply_substitutions_from_file(folder_names, file_unify)
+  # Split the folder names at underscore or space into words
+  words <- kwb.fakin:::sort_unique(
+    tolower(unlist(strsplit(folder_names, "[_ ]", folder_names)))
+  )
 
-  sort_unique(folder_names)
-
-  words <- sort_unique(tolower(unlist(strsplit(folder_names, "[_ ]", folder_names))))
-
-  kwb.utils::catLines(words)
+  # Print the ordered list of words
+  writeLines(words)
 
   # "(\\d+)(st|nd|rd|th)" = c(volume = 1),
   # "(Bogota|Sofia|Montbeliard|Braunschweig)" = c(city = 1),
@@ -125,16 +150,8 @@ if (FALSE)
   attributes[grep("@", attributes, invert = TRUE)]
 }
 
-# MAIN: Clear storage ----------------------------------------------------------
-if (FALSE)
-{
-  kwb.fakin:::clear_storage("paths")
-  kwb.fakin:::clear_storage("path_tree")
-}
-
-
 # provide_path_tree ------------------------------------------------------------
-provide_path_tree <- function(file)
+provide_path_tree <- function(file, fileEncoding)
 {
   THIS_SCRIPT <- "extract_properties"
 
@@ -146,14 +163,18 @@ provide_path_tree <- function(file)
   # Try to restore a vector of folder paths or recreate and store it
   if (fails(paths <- kwb.fakin:::restore("paths"))) {
 
-    paths_raw <- kwb.fakin::read_paths(file, fileEncoding = "Windows-1252")
-    # encoding = "UTF-8"
-    paths <- kwb.file::remove_common_root(paths_raw)
+    paths <- kwb.file::remove_common_root(kwb.fakin::read_paths(
+      file, encoding = "UTF-8", fileEncoding = fileEncoding
+    ))
+
     kwb.fakin:::store(paths, THIS_SCRIPT)
   }
 
   path_tree <- kwb.fakin:::to_tree(paths)
+
   kwb.fakin:::store(path_tree, THIS_SCRIPT)
+
+  path_tree
 }
 
 # fails ------------------------------------------------------------------------
@@ -179,6 +200,24 @@ grep_frequency <- function(f, p)
 {
   names <- rownames(f)
   f[grepl(p, names, ignore.case = TRUE), , drop = FALSE]
+}
+
+
+# plot_top_properties_distribution ---------------------------------------------
+plot_top_properties_distribution <- function(result)
+{
+  # Restore current graphic parameters on exit
+  old_par <- par(no.readonly = TRUE)
+  on.exit(par(old_par))
+
+  # Generate property strings and show the most frequent properties
+  frequencies <- sort(table(sapply(result, to_property_strings)))
+
+  # Set margins for a barplot of the frequencies
+  kwb.plot::setMargins(left = 14, top = 0, bottom = 2)
+
+  # Plot the frequencies
+  barplot(tail(frequencies, 30), horiz = TRUE, cex.names = 0.6, las = 1)
 }
 
 # TEST AREA --------------------------------------------------------------------
