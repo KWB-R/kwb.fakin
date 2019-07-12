44edf3a925a52957535c1b053fb242cb5a37a4a0
hsonne <hauke.sonnenberg@gmx.de>
2019-07-12 21:50:10 +0200

    Move apply_substitutions_from_file() to its own file
    also helper function read_substitutions_from_file()

--- R/path_properties.R
+++ R/path_properties.R
@@ -1,26 +1,7 @@
-
 # print_replacement_template ---------------------------------------------------
 print_replacement_template <- function(x)
 {
   kwb.utils::catLines(paste(x, x, sep = ";"))
-}
-
-# apply_substitutions_from_file ------------------------------------------------
-apply_substitutions_from_file <- function(x, file, dbg = TRUE)
-{
-  y <- kwb.utils::multiSubstitute(x, read_substitutions_from_file(file))
-
-  cat_changes_if(dbg, x, y)
-
-  y
-}
-
-# read_substitutions_from_file -------------------------------------------------
-read_substitutions_from_file <- function(file)
-{
-  substitution_data <- read_csv_de(file)
-
-  kwb.utils::toLookupList(data = substitution_data)
 }
 
 # get_words_to_attribute_list --------------------------------------------------
