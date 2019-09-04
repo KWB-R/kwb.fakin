37da8e5444e5137f818d94e7f1565be54897c5fa
hsonne <hauke.sonnenberg@gmx.de>
2019-09-04 22:02:39 +0200

    Improve tests to reach a code coverage of about 85%

--- tests/testthat/test-function-github_package_files.R
+++ tests/testthat/test-function-github_package_files.R
@@ -3,8 +3,8 @@ test_that("github_package_files() works", {
   f <- kwb.fakin:::github_package_files
 
   expect_error(f())
-  expect_error(f("repo", "path"))
-  expect_error(f("kwb-r/kwb.utils", "no such file"))
+  expect_warning(expect_error(f("repo", "path")))
+  expect_warning(expect_error(f("kwb-r/kwb.utils", "no such file")))
 
   repo <- "kwb-r/kwb.utils"
   expect_identical(f(repo, "DESCRIPTION"), "DESCRIPTION")
--- tests/testthat/test-function-prepare_for_scatter_plot.R
+++ tests/testthat/test-function-prepare_for_scatter_plot.R
@@ -14,3 +14,20 @@ test_that("prepare_for_scatter_plot() works", {
   f(file_data = file_data, n_root_parts = 1, min_depth = 1)
   f(file_data = file_data, n_root_parts = 1, start_path = "a")
 })
+
+test_that("prepare_for_scatter_plot2() works", {
+
+  f <- kwb.fakin:::prepare_for_scatter_plot2
+
+  expect_error(f())
+
+  file_data <- pathlist::pathlist(
+    c("a/b/c", "a/b/d"), data = data.frame(size = c(1, 2), type = "file")
+  )
+
+  file_data2 <- file_data
+  file_data2@data$type <- "directory"
+
+  f(file_data = file_data)
+  expect_null(f(file_data = file_data2))
+})
