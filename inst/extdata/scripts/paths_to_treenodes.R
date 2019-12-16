1415918b7104a66777494b67d1ed233d4222c0cd
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2019-12-16 17:35:01 +0100

    Clean paths_to_treenodes.R
    - Do not create a pathlist object (not used)
    - Improve names ("subdirs" instead of "x")
    - Move get_node_positions() down

--- inst/extdata/scripts/paths_to_treenodes.R
+++ inst/extdata/scripts/paths_to_treenodes.R
@@ -5,6 +5,8 @@ if (FALSE)
   file_info_dir <- "//medusa/processing/CONTENTS/file-info_by-department/2019-12/"
   path_list <- kwb.fakin:::read_path_information(file_info_dir)
   paths <- path_list$`path-info-ps-1_20191201_SUW_Department`$path
+  #kwb.fakin:::store(paths, "paths_to_treenodes")
+  #paths <- kwb.fakin:::restore("paths", index = 1)
 
   # - Or: simple test paths
   paths <- c("a/b/a", "a/b/c", "a/c/a", "a/b/a/x")
@@ -19,29 +21,31 @@ if (FALSE)
 if (FALSE)
 {
   # Provide a matrix of subdirectory names
-
-  # - 1. implicitly using pathlist
-  pl <- pathlist::pathlist(paths)
-  x <- pl@folders
-
-  # - 2. explicitly using to_subdir_matrix()
   #remotes::install_github("kwb-r/kwb.file@dev")
-  system.time(x1 <- kwb.file::to_subdir_matrix(paths, method = 1))
-  system.time(x2 <- kwb.file::to_subdir_matrix(paths, method = 2))
+  system.time(subdirs_1 <- kwb.file::to_subdir_matrix(paths, method = 1))
+  system.time(subdirs_2 <- kwb.file::to_subdir_matrix(paths, method = 2))
 
-  identical(x1, x2)
-  x <- x1
+  # Check the subdirectory matrices for identity
+  identical(subdirs_1, subdirs_2)
 
-  # Number of paths
-  nrow(x)
+  # Continue with either subdirectory matrix
+  subdirs <- subdirs_1
+
+  # Number of rows = number of paths, number of columns = max depth level
+  dim(subdirs)
 
   # Create a network structure (with elements nodes and edges)
-  system.time(network <- create_network(x))
-  system.time(network2 <- create_network(x, method = 2))
-  identical(network, network2)
-  #diffobj::diffStr(network, network2)
+  system.time(network_1 <- create_network(subdirs, method = 1))
+  system.time(network_2 <- create_network(subdirs, method = 2))
 
-  nets <- lapply(2:4, function(depth) prune_network(network, depth = depth))
+  identical(network_1, network_2)
+  #diffobj::diffStr(network_1, network_2)
+
+  # Continue with either network
+  network <- network_1
+
+  # Prune the network at different maximal depths
+  nets <- lapply(2:5, function(depth) prune_network(network, depth = depth))
 
   graphs <- lapply(nets, function(net) {
     igraph::make_graph(t(net$edges), directed = FALSE)
@@ -59,59 +63,19 @@ if (FALSE)
     )
   }
 
-  testtable <- data.frame(path = as.character(pl)[1:5000], value = 1)
+  testtable <- data.frame(path = paths[1:20], value = 1)
 
-  system.time(tree1 <- data.tree::FromDataFrameTable(testtable, pathName = "path"))
-  system.time(tree2 <- FromDataFrameTable(testtable, pathName = "path"))
+  system.time(tree <- data.tree::FromDataFrameTable(testtable, pathName = "path"))
 
-  identical(as.data.frame(tree1), as.data.frame(tree2))
-
-  plot(tree1)
-  plot(tree2)
-
-  data.tree::as.Node(as.data.frame(matrix(nrow = 2, byrow = TRUE, c(
-    "A", "B", "C.txt",
-    "A", "D", "E.txt"
-  ))))
+  plot(tree)
 }
 
 # Find nodes -------------------------------------------------------------------
 if (FALSE)
 {
-  x <- pl@folders
-  x <- kwb.file::to_subdir_matrix(kwb.pathdict:::random_paths(max_depth = 5))
-  dim(x)
-
-  system.time(node_pos2 <- get_node_positions(x))
-  identical(node_pos, node_pos2)
+  system.time(node_pos <- get_node_positions(x))
 
   t(node_pos)
-  x
-}
-
-# get_node_positions -----------------------------------------------------------
-get_node_positions <- function(x)
-{
-  stopifnot(is.matrix(x), nrow(x) > 0)
-
-  next_equals <- x[-nrow(x), ] == x[-1, ]
-
-  n_col <- ncol(x)
-
-  row_of_false <- matrix(nrow = 1, rep(FALSE, n_col))
-
-  next_equals <- rbind(row_of_false, next_equals)
-
-  do.call(rbind, lapply(seq_len(n_col), function(j) {
-
-    message(j, "/", n_col)
-
-    cols <- next_equals[, seq_len(j), drop = FALSE]
-
-    i <- which(! Reduce("&", kwb.utils::asColumnList(cols)))
-
-    matrix(c(i, rep(j, length(i))), ncol = 2)
-  }))
 }
 
 # create_network ---------------------------------------------------------------
@@ -189,4 +153,31 @@ prune_network <- function(network, depth = 2)
   network$edges <- edges[edges[, 2] %in% node_ids, ]
 
   network
+}
+
+# get_node_positions -----------------------------------------------------------
+get_node_positions <- function(x)
+{
+  kwb.utils::stopIfNotMatrix(x)
+
+  stopifnot(nrow(x) > 0)
+
+  next_equals <- x[-nrow(x), ] == x[-1, ]
+
+  n_col <- ncol(x)
+
+  row_of_false <- matrix(nrow = 1, rep(FALSE, n_col))
+
+  next_equals <- rbind(row_of_false, next_equals)
+
+  do.call(rbind, lapply(seq_len(n_col), function(j) {
+
+    message(j, "/", n_col)
+
+    cols <- next_equals[, seq_len(j), drop = FALSE]
+
+    i <- which(! Reduce("&", kwb.utils::asColumnList(cols)))
+
+    matrix(c(i, rep(j, length(i))), ncol = 2)
+  }))
 }
