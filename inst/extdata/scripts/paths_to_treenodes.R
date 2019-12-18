b4e4bbdb282cbed4b075cc451637f0f513202903
hsonne <hauke.sonnenberg@gmx.de>
2019-12-18 15:10:22 +0100

    Create network from paths, not from subdirs any more
    - remove create_network(), unify_net()
    - do not support old structure in prune_network()

--- inst/extdata/scripts/paths_to_treenodes.R
+++ inst/extdata/scripts/paths_to_treenodes.R
@@ -20,134 +20,39 @@ if (FALSE)
 # MAIN -------------------------------------------------------------------------
 if (FALSE)
 {
-  # Provide a matrix of subdirectory names
   #remotes::install_github("kwb-r/kwb.file@dev")
-  system.time(subdirs <- kwb.file::to_subdir_matrix(paths))
 
+  # Provide a matrix of subdirectory names
+  #system.time(subdirs <- kwb.file::to_subdir_matrix(paths))
   # Number of rows = number of paths, number of columns = max depth level
-  dim(subdirs)
+  #dim(subdirs)
+  # Convert matrix back to paths
+  # paths <- subdir_matrix_to_paths(subdirs)
 
   # Create a network structure (with elements nodes and edges)
-  system.time(network_1 <- create_network(subdirs, method = 1))
-  system.time(network_2 <- create_network(subdirs, method = 2))
+  system.time(network <- get_nodes_and_edges(paths))
 
-  identical(unify_net(network_1), unify_net(network_2))
   #diffobj::diffStr(network_1, network_2)
-
-  net_1 <- prune_network(network_1)
-  net_2 <- prune_network(network_2)
-
-  identical(unify_net(net_1), unify_net(net_2))
-
-  # Continue with either network
-  network <- network_2
+  net_1 <- prune_network(network)
 
   # Prune the network at different maximal depths
   nets <- lapply(2:4, function(depth) prune_network(network, depth = depth))
 
   graphs <- lapply(nets, network_to_igraph)
 
-  plot(graphs[[3]])
+  plot(graphs[[2]])
 
-  for (i in seq_along(graphs)) {
-    net <- nets[[i]]
-    plot(
-      graphs[[i]],
-      vertex.color = "red",
-      vertex.size = 2,
-      vertex.label.cex = 0.6
-      , vertex.label = net$nodes$name[unique(as.integer(net$edges))]
-      , layout = igraph::layout.kamada.kawai
-    )
-  }
+  plot(
+    graphs[[2]], vertex.color = "red", vertex.size = 2, vertex.label.cex = 0.6
+  )
 
-  testtable <- data.frame(path = paths[1:20], value = 1)
+  igraph::as_data_frame(graphs[[2]], what = "vertices")
+
+  testtable <- data.frame(path = paths[1:15], value = 1)
 
   system.time(tree <- data.tree::FromDataFrameTable(testtable, pathName = "path"))
 
   plot(tree)
-}
-
-# create_network ---------------------------------------------------------------
-create_network <- function(x, method = 1)
-{
-  kwb.utils::stopIfNotMatrix(x)
-
-  if (method == 2) {
-    return(get_nodes_and_edges(paths = subdir_matrix_to_paths(x)))
-  }
-
-  # Initialise a matrix of (tree node) IDs
-  ids <- matrix(integer(), nrow = nrow(x), ncol = ncol(x))
-
-  # Maximal ID so far
-  max_id <- 0
-
-  # Node names occurring in the different path depths
-  node_names <- list()
-
-  # Loop through the columns of the subdirectory matrix
-  for (j in seq_len(ncol(x))) {
-
-    #j <- 3
-    message("Giving IDs to nodes in depth ", j, "/", ncol(x))
-
-    # Which rows have non-empty values in the current column?
-    rows <- which(nzchar(x[, j]))
-
-    # Values in the current column j
-    xj <- x[rows, j]
-
-    # Prepend node IDs of parents unless this is the first column
-    xx <- if (j == 1) xj else paste(ids[rows, j - 1], xj, sep = "-")
-
-    # Give new IDs to unique combinations of parent (if any) and value
-    new_ids <- match(xx, unique(xx))
-
-    # Store the node names in the list
-    node_names[[j]] <- xj[diff(c(0, new_ids)) > 0]
-
-    # Store the new IDs in the ID matrix
-    ids[rows, j] <- new_ids + max_id
-
-    # Update maximal ID
-    max_id <- max_id + max(new_ids)
-  }
-
-  # Generate edges between start-node and end-node
-  n_col <- ncol(ids)
-
-  # Generate edges between start-node and end-node
-  edges <- do.call(rbind, lapply(seq.int(2L, n_col, by = 1L), function(j) {
-    message("Creating edges to depth ", j, "/", n_col)
-    unique(ids[! is.na(ids[, j]), c(j - 1L, j)])
-  }))
-
-  nodes <- kwb.utils::rbindAll(
-    x = lapply(node_names, function(name) {
-      data.frame(name = name, stringsAsFactors = FALSE)
-    }),
-    nameColumn = "depth",
-    namesAsFactor = FALSE
-  )
-
-  list(nodes = nodes, edges = edges)
-}
-
-# unify_net --------------------------------------------------------------------
-unify_net <- function(net)
-{
-  net$nodes <- kwb.utils::selectColumns(net$nodes, c(id = "name", "depth"))
-  net$nodes$name <- gsub("^//", "", net$nodes$name)
-
-  if (is.matrix(net$edges)) {
-    net$edges <- data.frame(
-      node = as.integer(net$edges[, 2]),
-      parent = as.integer(net$edges[, 1])
-    )
-  }
-
-  net
 }
 
 # prune_network ----------------------------------------------------------------
@@ -156,14 +61,7 @@ prune_network <- function(network, depth = 2)
   nodes <- kwb.utils::selectElements(network, "nodes")
   edges <- kwb.utils::selectElements(network, "edges")
 
-  edge_nodes <- if (is.matrix(edges)) {
-    stopifnot(ncol(edges) >= 2)
-    edges[, 2]
-  } else if (is.data.frame(edges)) {
-    kwb.utils::selectColumns(edges, "node")
-  } else {
-    stop("network$nodes must be a matrix or a data frame!", call. = FALSE)
-  }
+  edge_nodes <- kwb.utils::selectColumns(edges, "node")
 
   node_ids <- which(kwb.utils::selectColumns(nodes, "depth") <= depth)
 
